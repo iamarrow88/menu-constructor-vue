e5c5255f3714c17df694615bc7504842d88e7a16
Nastya Pisanko <nana.pisanka@gmail.com>
2025-07-10 02:56:37 +0300

    feat: add text content

--- menu-constructor-vue/src/views/NotFoundPage.vue
+++ menu-constructor-vue/src/views/NotFoundPage.vue
@@ -3,13 +3,3 @@
     <h1>404 Page</h1>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .not-found-page {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
